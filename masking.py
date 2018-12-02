bcd5d9f95c91833da3b1054aed71774208a903ff
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-12-02 14:50:30 +0530

    Update masking.py

--- masking.py
+++ masking.py
@@ -2,32 +2,29 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-# Read in the image
+
 image = mpimg.imread('test.jpg')
 
-# Grab the x and y size and make a copy of the image
+
 ysize = image.shape[0]
 xsize = image.shape[1]
 color_select = np.copy(image)
 line_image = np.copy(image)
 
-# Define color selection criteria
-# MODIFY THESE VARIABLES TO MAKE YOUR COLOR SELECTION
+
 red_threshold = 200
 green_threshold = 200
 blue_threshold = 200
 
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
-# Define the vertices of a triangular mask.
-# Keep in mind the origin (x=0, y=0) is in the upper left
-# MODIFY THESE VALUES TO ISOLATE THE REGION 
-# WHERE THE LANE LINES ARE IN THE IMAGE
+# Defining the vertices of a triangular mask.
+
 left_bottom = [50, 539]
 right_bottom = [900, 539]
 apex = [450, 290]
 
-# Perform a linear fit (y=Ax+B) to each of the three sides of the triangle
+# Performing a linear fit (y=Ax+B) to each of the three sides of the triangle
 # np.polyfit returns the coefficients [A, B] of the fit
 fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
 fit_right = np.polyfit((right_bottom[0], apex[0]), (right_bottom[1], apex[1]), 1)
